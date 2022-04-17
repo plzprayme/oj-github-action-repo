c21e0dd2db86ed5b051888944ec97344603659f4
prayme <dbfpzk142@gmail.com>
2022-04-17 23:31:48 +0900

    do

--- mkdir.sh
+++ mkdir.sh
@@ -11,4 +11,9 @@ mkdir 문자열 && cd 문자열
 touch 11022.java
 touch 1260.java
 touch 1011.java
+cd ../..
 
+mkdir leetcode && cd leetcode
+touch increasing-order-search-tree.go
+mkdir easy && cd easy
+touch palindrome-number.go
